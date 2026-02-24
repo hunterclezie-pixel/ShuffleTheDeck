0578da11025b5c53fc65c93de5fa68b12d90358e
Hunter Clezie <clezhunt@gmail.com>
2026-02-23 19:03:38 -0700

    add display

--- ShuffleTheDeck/Program.cs
+++ ShuffleTheDeck/Program.cs
@@ -7,20 +7,62 @@ github url: https://github.com/hunterclezie-pixel/ShuffleTheDeck.git
 */
 namespace ShuffleTheDeck
 {
-    //Todo list:
-    // Write a program that deals a card from a standard deck of 52 playing cards.
-    // The card should be a random suit (spades, clubs, hearts, diamonds) and value (2-10, J, Q, K, A).
-    // Use a multidimensional array to track if the card has already been dealt. If so, draw another random card.
-    // Shuffle the deck when there are no more cards left or anytime the user chooses.
+    // Todo list:
+    // [] Write a program that deals a card from a standard deck of 52 playing cards.
+    // [] The card should be a random suit (spades, clubs, hearts, diamonds) and value (2-10, J, Q, K, A).
+    // [] Use a multidimensional array to track if the card has already been dealt. If so, draw another random card.
+    // [] Shuffle the deck when there are no more cards left or anytime the user chooses.
 
     internal class Program
     {
+        static bool[,] drawnCards = new bool[4, 13];
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Display();
 
             //pause
             Console.Read();
+
+        }
+
+        static void Display()
+        {
+            int padding = 8;
+            int prettyNumber = 0;
+            string placeHolder = "";
+            string columnSeperator = "  |";
+            string currentRow = "";
+            //print heading row
+            string[] heading = { "Spades", "Clubs", "Hearts", "Diamonds"};
+            foreach (string thing in heading)
+            {
+                Console.Write(thing.PadLeft(padding) + columnSeperator);
+            }
+            Console.WriteLine();
+
+            //print the rest of the rows
+            for (int number = 1; number <= 13; number++)
+            {
+
+                //assemble the row
+                for (int suits = 0; suits < 4; suits++)
+                {
+                    if (drawnCards[suits, number - 1])
+                    {
+                        prettyNumber = number + (suits * 13);
+                        currentRow += prettyNumber.ToString().PadLeft(padding) + columnSeperator;
+                    }
+                    else
+                    {
+                        currentRow += placeHolder.PadLeft(padding) + columnSeperator;
+
+                    }
+                }
+                Console.WriteLine(currentRow);
+                currentRow = ""; //Resets the row for the next iteration    
+            }
+
         }
     }
 }
