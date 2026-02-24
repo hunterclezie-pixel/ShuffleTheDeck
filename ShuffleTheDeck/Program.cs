1cd7ae84222076666b55e1629c31601d33bf0b52
Hunter Clezie <clezhunt@gmail.com>
2026-02-23 18:26:38 -0700

    init

--- ShuffleTheDeck/Program.cs
+++ ShuffleTheDeck/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace ShuffleTheDeck
+﻿/* 
+Hunter Clezie 
+Spring 2026
+RCET2265
+ShuffleTheDeck
+github url: https://github.com/hunterclezie-pixel/ShuffleTheDeck.git
+*/
+namespace ShuffleTheDeck
 {
     internal class Program
     {
