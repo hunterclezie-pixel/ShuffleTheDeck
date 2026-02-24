9f7eecbed4f6c91da1b4d27a04e63478967931e0
Hunter Clezie <clezhunt@gmail.com>
2026-02-23 18:25:27 -0700

    Add project files.

---
+++ ShuffleTheDeck/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShuffleTheDeck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
