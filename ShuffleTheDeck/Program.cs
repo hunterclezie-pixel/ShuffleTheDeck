dc62e964033e569306c228f7a69ba05472d3a3bf
Hunter Clezie <clezhunt@gmail.com>
2026-02-23 18:35:49 -0700

    add todo list and console read function

--- ShuffleTheDeck/Program.cs
+++ ShuffleTheDeck/Program.cs
@@ -7,11 +7,20 @@ github url: https://github.com/hunterclezie-pixel/ShuffleTheDeck.git
 */
 namespace ShuffleTheDeck
 {
+    //Todo list:
+    // Write a program that deals a card from a standard deck of 52 playing cards.
+    // The card should be a random suit (spades, clubs, hearts, diamonds) and value (2-10, J, Q, K, A).
+    // Use a multidimensional array to track if the card has already been dealt. If so, draw another random card.
+    // Shuffle the deck when there are no more cards left or anytime the user chooses.
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //pause
+            Console.Read();
         }
     }
 }
